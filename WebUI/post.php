f4121321adb33be5ecb4f02d5a489737fbbb38bc
shan-mathi <67865135+shan-mathi@users.noreply.github.com>
2021-02-14 22:18:40 +0530

    Update post.php

--- WebUI/post.php
+++ WebUI/post.php
@@ -5,6 +5,7 @@ if(isset($_SESSION['name'])){
      
     $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
     file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
+	file_put_contents("question.txt", stripslashes(htmlspecialchars($text))."\n", FILE_APPEND | LOCK_EX);
 	//TODO Integrate the API pass the question and get the anser. write the answer into below file
 	//example
 	/*
@@ -24,11 +25,12 @@ if(isset($_SESSION['name'])){
 	}
 	$answer=$decoded->response;
 	*/
-	$answer="Im fine";//TODO replace this with the response from API response
+	//TODO Uncomment below line for immediate answer for the question
+	/*$answer="Im fine";//TODO replace this with the response from API response
 
 	$question="<div class='msgln'><b class='user-name'>Q:</b>".stripslashes(htmlspecialchars($text))."<br>";
-	$answer="Im fine";//TODO replace this with the response from API
+	
 	$question=$question."<b class='user-name-left'>A:</b>".$answer."<br></div>";
-	file_put_contents("response.html",$question,FILE_APPEND | LOCK_EX);
+	file_put_contents("response.html",$question,FILE_APPEND | LOCK_EX); */
 }
 ?>
