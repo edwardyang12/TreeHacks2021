ce4718f492b46c5264932cc49143e41bc68751d1
shan-mathi <67865135+shan-mathi@users.noreply.github.com>
2021-02-14 18:18:00 +0530

    main php file and style sheet upload for UI

---
+++ src/post.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if(isset($_SESSION['name'])){
+    $text = $_POST['text'];
+     
+    $text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
+    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
+	//TODO Integrate the API pass the question and get the anser. write the answer into below file
+	//example
+	/*
+	$service_url = 'https://example.com/api/conversations/[CONV_CODE]/messages&apikey=[API_KEY]';
+	$curl = curl_init($service_url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$curl_response = curl_exec($curl);
+	if ($curl_response === false) {
+		$info = curl_getinfo($curl);
+		curl_close($curl);
+		die('error occured during curl exec. Additioanl info: ' . var_export($info));
+	}
+	curl_close($curl);
+	$decodedResp = json_decode($curl_response);
+	if (isset($decodedResp->response->status) && $decodedResp->response->status == 'ERROR') {
+		die('error occured: ' . $decodedResp->response->errormessage);
+	}
+	$answer=$decoded->response;
+	*/
+	$answer="Im fine";//TODO replace this with the response from API response
+
+	$question="<div class='msgln'><b class='user-name'>Q:</b>".stripslashes(htmlspecialchars($text))."<br>";
+	$answer="Im fine";//TODO replace this with the response from API
+	$question=$question."<b class='user-name-left'>A:</b>".$answer."<br></div>";
+	file_put_contents("response.html",$question,FILE_APPEND | LOCK_EX);
+}
+?>
