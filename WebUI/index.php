d42890580ce738d0b0ee405ef09dc9847cfd16f0
shan-mathi <67865135+shan-mathi@users.noreply.github.com>
2021-02-14 22:20:10 +0530

    update index.php

--- WebUI/index.php
+++ WebUI/index.php
@@ -39,17 +39,16 @@ function loginForm(){
 <html lang="en">
     <head>
 
- <title>TreeHacks-Ask ME.AI</title>
+ <title>AskME.AI</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<!--<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat"> 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 <link href="style.css" rel="stylesheet">
 <style>
     body,
@@ -80,8 +79,12 @@ function loginForm(){
 	   <header style="padding:128px 16px"  >
         <div class="img-back"></div>
         <h1 class="askme">AskMe.AI</h1>
+		<br><br>
         <p class="treehacks">TreeHacks 2021</p>
-        <button>Start</button>
+		<form name="process" action="">           
+               <input class="button" name="processQ" type="submit" id="processQ" value="Process" />
+        </form>
+        
       </header>
     
       <br>
